4e1d8f4c8e5177d34c2cf7853776ac8631f1d76e
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-08-26 17:01:33 -0500

    commented out schema due to use of sequelize

--- schema.sql
+++ schema.sql
@@ -1,30 +1,30 @@
-DROP DATABASE IF EXISTS pokemon;
+-- DROP DATABASE IF EXISTS pokemon;
 
-CREATE DATABASE pokemon;
+-- CREATE DATABASE pokemon;
 
-USE pokemon;
+-- USE pokemon;
 
-CREATE TABLE IF NOT EXISTS Users (
-  id int NOT NULL AUTO_INCREMENT,
-  username varchar(255) NOT NULL,
-  record  int NOT NULL,
-  PRIMARY KEY (ID)
-);
+-- CREATE TABLE IF NOT EXISTS Users (
+--   id int NOT NULL AUTO_INCREMENT,
+--   username varchar(255) NOT NULL,
+--   record  int NOT NULL,
+--   PRIMARY KEY (ID)
+-- );
 
-CREATE TABLE IF NOT EXISTS Pokemon (
-  id int NOT NULL AUTO_INCREMENT,
-  pokename varchar(255) NOT NULL,
-  descript varchar(255) NOT NULL,
-  imageurl varchar(255) NOT NULL,
-  PRIMARY KEY (ID)
-);
+-- CREATE TABLE IF NOT EXISTS Pokemon (
+--   id int NOT NULL AUTO_INCREMENT,
+--   pokename varchar(255) NOT NULL,
+--   descript varchar(255) NOT NULL,
+--   imageurl varchar(255) NOT NULL,
+--   PRIMARY KEY (ID)
+-- );
 
-CREATE TABLE IF NOT EXISTS users_pokemon (
-  user_id int NOT NULL,
-  pokemon_id int NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES Users(ID),
-  FOREIGN KEY (pokemon_id) REFERENCES Pokemon(ID)
-);
+-- CREATE TABLE IF NOT EXISTS users_pokemon (
+--   user_id int NOT NULL,
+--   pokemon_id int NOT NULL,
+--   FOREIGN KEY (user_id) REFERENCES Users(ID),
+--   FOREIGN KEY (pokemon_id) REFERENCES Pokemon(ID)
+-- );
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
