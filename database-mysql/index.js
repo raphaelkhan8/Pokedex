a1ed67e70fb11b502eab4e7210e224e00dc0c581
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-08-26 15:34:54 -0500

    setup database and connection using sequelize

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,20 +1,82 @@
-const mysql = require('mysql');
+const sequelize = require('sequelize');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'FILL_ME_IN',
-  database: 'test',
+// set up the database connection
+const poke = new Sequelize('pokemon', 'root', '', {
+  host: '',
+  port: 3306,
+  dialect: 'mysql',
 });
 
-const selectAll = (callback) => {
-  connection.query('SELECT * FROM items', (err, items) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, items);
-    }
+// test the db connection
+poke.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
   });
-};
 
-module.exports.selectAll = selectAll;
+// make a Users table
+const Users = sequelize.define('users', {
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+}, {
+  timestamps: false,
+});
+
+
+// make a pokemon table
+const Pokemon = sequelize.define('pokemon', {
+  pokename: {
+    type: Sequelize.STRING,
+  },
+  powerLevel: {
+    type: Sequelize.INTEGER(11),
+  },
+  descript: {
+    type: Sequelize.STRING,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+  },
+});
+
+
+// make a joint users/pokemon table
+const UsersPokemon = sequelize.define('users_pokemon', {
+  userId: {
+    type: Sequelize.INTEGER(11),
+  },
+  pokeId: {
+    type: Sequelize.INTEGER(11),
+  },
+});
+
+
+module.exports.Users = Users;
+module.exports.Pokemon = Pokemon;
+module.exports.UsersPokemon = UsersPokemon;
+
+// mySQL connection without sequlize:
+// const mysql = require('mysql');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'FILL_ME_IN',
+//   database: 'test',
+// });
+
+// const selectAllPokemon = (callback) => {
+//   connection.query('SELECT * FROM Pokemon', (err, items) => {
+//     if (err) {
+//       callback(err, null);
+//     } else {
+//       callback(null, items);
+//     }
+//   });
+// };
+
+// module.exports.selectAllPokemon = selectAllPokemon;
