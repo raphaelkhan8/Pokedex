fe59aaacee7fa971a2e48fb9003450b7291cf520
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-08-26 22:46:32 -0500

    set up search route/recieving data from api

--- server/index.js
+++ server/index.js
@@ -30,9 +30,29 @@ app.post('/sign-in/:user', (req, res) => {
 // route for when search button is clicked
 // GET request gets the input pokemon's info from pokeapi
 app.get('/search', (req, res) => {
-  axios.get(`https://pokeapi.co/api/v2/pokemon/${input_pokemon(CHANGE_MEEEEEEEE)}`)
-    .then()
-    .catch();
+  const { name } = req.query;
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    // response is an object which a data property that is an object with all the data we want
+    // need to get back following properties: name, base_experience, sprites and species.url
+    // species.url contains the pokemon's description but in a ton of lanhuages so need to filter
+    // out english translated description
+    .then((response) => {
+      console.log(response.data);
+      console.log(response.data.name);
+      console.log(response.data.base_experience);
+      console.log(response.data.sprites.front_default);
+      console.log(response.data.species.url);
+      console.log('hiiii');
+      const pokeData = {};
+      pokeData.name = response.data;
+      pokeData.powerLevel = response.data.base_experience;
+      pokeData.imageUrl = response.data.sprites.front_default;
+      pokeData.description = response.data.species.url;
+      res.json(pokeData);
+    })
+    .catch((err) => {
+      console.error('Error when getting data from api. See line 54 server/index.js', err);
+    });
 });
 
 // route for when GET POKEMON button is clicked
