b16293a557aa6df3875bdc44f2f36538f36870ca
unknown <raphaelkhan8@gmail.com>
2020-10-04 12:35:26 -0500

    route for adding first pokemon

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,180 +1,265 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
-import List from './components/List.jsx';
+import List from "./components/List.jsx";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userInput: '',
-      searched: false,
-      hasError: false,
-      userId: '',
-      username: '',
-      query: '',
-      pokemon: {
-        name: '',
-        type: '',
-        powerLevel: 0,
-        description: '',
-        imageUrl: '',
-      },
-      pokeItems: [],
-    };
-    this.handleInput = this.handleInput.bind(this);
-    this.handleSignIn = this.handleSignIn.bind(this);
-    this.handleSearch = this.handleSearch.bind(this);
-    this.addPokemon = this.addPokemon.bind(this);
-    this.changeLeadPokemon = this.changeLeadPokemon.bind(this);
-  }
-
-  componentDidMount() {
-    const { query } = this.state;
-    if (query.length) {
-      this.handleSearch();
+    constructor(props) {
+        super(props);
+        this.state = {
+            userInput: "",
+            searched: false,
+            hasError: false,
+            userId: "",
+            username: "",
+            query: "",
+            pokemon: {
+                name: "",
+                type: "",
+                powerLevel: 0,
+                description: "",
+                imageUrl: "",
+            },
+            pokeItems: [],
+        };
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSignIn = this.handleSignIn.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.addPokemon = this.addPokemon.bind(this);
+        this.changeLeadPokemon = this.changeLeadPokemon.bind(this);
     }
-  }
 
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
-
-  handleInput(event) {
-    (event.target.id === 'signin-bar')
-      ? this.setState({ userInput: event.target.value })
-      : this.setState({ query: event.target.value });
-  }
-
-  handleSignIn() {
-    const { userInput } = this.state;
-    axios.post('/sign-in', { userInput })
-      .then((response) => {
-        this.setState({
-          userId: response.data.id,
-          username: userInput,
-        });
-        return response;
-      })
-      .then((respo) => {
-        const { id } = respo.data;
-        axios.get(`/pokemon/${id}`)
-          .then((res) => {
-            this.setState({
-              pokeItems: res.data,
-              userInput: '',
-            });
-          });
-      })
-      .catch((err) => {
-        console.error('Error while signing in.', err);
-      });
-  }
-
-  handleSearch() {
-    let { query } = this.state;
-    query = query.toLowerCase();
-    return axios.get(`/search/?name=${query}`)
-      .then((res) => {
-        if (!res.data) {
-          this.setState({
-            hasError: true,
-          });
-        } else {
-          this.setState({
-            pokemon: res.data,
-            searched: true,
-            query: '',
-          });
+    componentDidMount() {
+        const { query } = this.state;
+        if (query.length) {
+            this.handleSearch();
         }
-      }).catch((err) => {
-        console.error('Search error.', err);
-      });
-  }
-
-  addPokemon() {
-    const { userId, pokemon, pokeItems } = this.state;
-    let leadName;
-    const leadPokemon = pokeItems[0] || { pokemon: { powerLevel: 1000 } };
-    const pokemonName = pokemon.name[0].toUpperCase().concat(pokemon.name.slice(1));
-    if (pokeItems.length) {
-      leadName = leadPokemon.pokemon.name[0].toUpperCase().concat(leadPokemon.pokemon.name.slice(1));
     }
-    axios.post('/pokebattle', { userId, pokemon, leadPokemon })
-      .then((res) => {
-        const battleMessage = res.data[0];
-        const battleExperience = res.data[res.data.length - 1];
-        (battleMessage === 'Caught!') ? window.alert(`Congrats! You caught ${pokemonName} \n${leadName} gained ${battleExperience} powerLevel points!`)
-          : window.alert(`${leadName} fainted D: \n${pokemonName} was too strong :(`);
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    handleInput(event) {
+        event.target.id === "signin-bar"
+            ? this.setState({ userInput: event.target.value })
+            : this.setState({ query: event.target.value });
+    }
+
+    handleSignIn() {
+        const { userInput } = this.state;
+        axios
+            .post("/sign-in", { userInput })
+            .then((response) => {
+                this.setState({
+                    userId: response.data.id,
+                    username: userInput,
+                });
+                return response;
+            })
+            .then((respo) => {
+                const { id } = respo.data;
+                axios.get(`/pokemon/${id}`).then((res) => {
+                    this.setState({
+                        pokeItems: res.data,
+                        userInput: "",
+                    });
+                });
+            })
+            .catch((err) => {
+                console.error("Error while signing in.", err);
+            });
+    }
+
+    handleSearch() {
+        let { query } = this.state;
+        query = query.toLowerCase();
+        return axios
+            .get(`/search/?name=${query}`)
+            .then((res) => {
+                if (!res.data) {
+                    this.setState({
+                        hasError: true,
+                    });
+                } else {
+                    this.setState({
+                        pokemon: res.data,
+                        searched: true,
+                        query: "",
+                    });
+                }
+            })
+            .catch((err) => {
+                console.error("Search error.", err);
+            });
+    }
+
+    addPokemon() {
+        const { userId, pokemon, pokeItems } = this.state;
+        if (!pokeItems.length) {
+            axios.post("/firstPokemon", { userId, pokemon }).then((res) => {
+                const message = res.data[0];
+                window.alert(message);
+                this.setState({
+                    pokeItems: res.data.slice(1, res.data.length - 1),
+                    searched: false,
+                });
+            });
+        } else {
+            const leadPokemon = pokeItems[0] || {
+                pokemon: { powerLevel: 1000 },
+            };
+            const leadName = leadPokemon.pokemon.name[0]
+                .toUpperCase()
+                .concat(leadPokemon.pokemon.name.slice(1));
+            const pokemonName = pokemon.name[0]
+                .toUpperCase()
+                .concat(pokemon.name.slice(1));
+            axios
+                .post("/pokebattle", { userId, pokemon, leadPokemon })
+                .then((res) => {
+                    const battleMessage = res.data[0];
+                    const battleExperience = res.data[res.data.length - 1];
+                    battleMessage === "Caught!"
+                        ? window.alert(
+                              `Congrats! You caught ${pokemonName} \n${leadName} gained ${battleExperience} powerLevel points!`
+                          )
+                        : window.alert(
+                              `${leadName} fainted D: \n${pokemonName} was too strong :(`
+                          );
+                    this.setState({
+                        pokeItems: res.data.slice(1, res.data.length - 1),
+                        searched: false,
+                    });
+                })
+                .catch((err) => {
+                    console.error("Adding Pokemon error", err);
+                });
+        }
+    }
+
+    changeLeadPokemon() {
+        const { pokeItems } = this.state;
+        const prevLead = pokeItems.shift();
+        pokeItems.push(prevLead);
         this.setState({
-          pokeItems: res.data.slice(1, res.data.length - 1),
-          searched: false,
+            pokeItems,
         });
-      })
-      .catch((err) => {
-        console.error('Adding Pokemon error', err);
-      });
-  }
+    }
 
-  changeLeadPokemon() {
-    const { pokeItems } = this.state;
-    const prevLead = pokeItems.shift();
-    pokeItems.push(prevLead);
-    this.setState({
-      pokeItems,
-    });
-  }
+    render() {
+        const {
+            searched,
+            hasError,
+            username,
+            userInput,
+            query,
+            pokemon,
+            pokeItems,
+        } = this.state;
 
-  render() {
-    const {
-      searched, hasError, username, userInput, query, pokemon, pokeItems,
-    } = this.state;
-
-    return (hasError) ? <h2>Sorry, your query is not a Pokemon. Refresh the page to try again.</h2>
-      : (
-        <div>
-          <h1>PokeDex</h1>
-          {!username.length
-            ? (
-              <div id="sign-in">
-                <h2>Sign In</h2>
-                <input type="text" id="signin-bar" value={userInput} onChange={this.handleInput} />
-                <button type="button" id="signin-button" onClick={this.handleSignIn}>Sign In</button>
-              </div>
-            ) : (<p><b>{username}</b> is signed-in.</p>)}
-          <div id="search">
-            <h2>Search for Pokemon</h2>
-            <input type="text" id="search-bar" value={query} onChange={this.handleInput} />
-            <button type="button" id="search-button" onClick={this.handleSearch}>Search</button>
-            {searched
-              ? (
-                <div>
-                  <h2>{pokemon.name.toUpperCase()}</h2>
-                  <img src={pokemon.imageUrl} alt="" />
-                  <p>{pokemon.description}</p>
-                  <h2>Type: {pokemon.type.toUpperCase()}</h2>
-                  <h2>Power Level: {pokemon.powerLevel}</h2>
-                  <button type="button" id="button-add-list" onClick={this.addPokemon}>Battle Pokemon to Add to my Party</button>
+        return hasError ? (
+            <h2>
+                Sorry, your query is not a Pokemon. Refresh the page to try
+                again.
+            </h2>
+        ) : (
+            <div>
+                <h1>PokeDex</h1>
+                {!username.length ? (
+                    <div id="sign-in">
+                        <h2>Sign In</h2>
+                        <input
+                            type="text"
+                            id="signin-bar"
+                            value={userInput}
+                            onChange={this.handleInput}
+                        />
+                        <button
+                            type="button"
+                            id="signin-button"
+                            onClick={this.handleSignIn}
+                        >
+                            Sign In
+                        </button>
+                    </div>
+                ) : (
+                    <p>
+                        <b>{username}</b> is signed-in.
+                    </p>
+                )}
+                <div id="search">
+                    <h2>Search for Pokemon</h2>
+                    <input
+                        type="text"
+                        id="search-bar"
+                        value={query}
+                        onChange={this.handleInput}
+                    />
+                    <button
+                        type="button"
+                        id="search-button"
+                        onClick={this.handleSearch}
+                    >
+                        Search
+                    </button>
+                    {searched ? (
+                        <div>
+                            <h2>{pokemon.name.toUpperCase()}</h2>
+                            <img src={pokemon.imageUrl} alt="" />
+                            <p>{pokemon.description}</p>
+                            <h2>Type: {pokemon.type.toUpperCase()}</h2>
+                            <h2>Power Level: {pokemon.powerLevel}</h2>
+                            <button
+                                type="button"
+                                id="button-add-list"
+                                onClick={this.addPokemon}
+                            >
+                                Battle Pokemon to Add to my Party
+                            </button>
+                        </div>
+                    ) : (
+                        <div />
+                    )}
                 </div>
-              )
-              : (<div />)}
-          </div>
-          {username.length ? (<h2>{username}'s Pokemon</h2>) : <h2>My Pokemon</h2>}
-          {(username.length && pokeItems.length > 1)
-            ? (<div>Lead Pokemon: {pokeItems.map(poke => poke.pokemon.name)[0]}</div>
-            && <button type="button" onClick={this.changeLeadPokemon}>Change Lead Pokemon</button>)
-            : (<div> </div>)}
-          {pokeItems.length
-            ? (<List pokeItems={pokeItems} />)
-            : (<div>You do not have any Pokemon. Search for one and add it to your collection.</div>)}
-          <div id="battle">
-            <h2>PokeTrade!!!</h2>
-            <img src="https://66.media.tumblr.com/cfd1b3a8a2fea38a086a0bf4549b0c3d/tumblr_p27s4aXmVE1s0dt2ao1_250.gif" alt="" />
-          </div>
-        </div>
-      );
-  }
+                {username.length ? (
+                    <h2>{`${username}'s Pokemon`}</h2>
+                ) : (
+                    <h2>My Pokemon</h2>
+                )}
+                {username.length && pokeItems.length > 1 ? (
+                    (
+                        <div>
+                            Lead Pokemon:{" "}
+                            {pokeItems.map((poke) => poke.pokemon.name)[0]}
+                        </div>
+                    ) && (
+                        <button type="button" onClick={this.changeLeadPokemon}>
+                            Change Lead Pokemon
+                        </button>
+                    )
+                ) : (
+                    <div> </div>
+                )}
+                {pokeItems.length ? (
+                    <List pokeItems={pokeItems} />
+                ) : (
+                    <div>
+                        You do not have any Pokemon. Search for one and add it
+                        to your collection.
+                    </div>
+                )}
+                <div id="battle">
+                    <h2>PokeTrade!!!</h2>
+                    <img
+                        src="https://66.media.tumblr.com/cfd1b3a8a2fea38a086a0bf4549b0c3d/tumblr_p27s4aXmVE1s0dt2ao1_250.gif"
+                        alt=""
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
--- server/router/routes/pokemon.js
+++ server/router/routes/pokemon.js
@@ -1,44 +1,60 @@
-const express = require('express');
+const express = require("express");
 const {
-  getUserPokemon, searchPokemon, addPokemonToCollection,
-} = require('../helpers/pokeHelpers');
-const { battle, gainBattleExperience } = require('../helpers/battleHelpers');
+    getUserPokemon,
+    searchPokemon,
+    addPokemonToCollection,
+} = require("../helpers/pokeHelpers");
+const { battle, gainBattleExperience } = require("../helpers/battleHelpers");
 
 const router = new express.Router();
 
-router.get('/pokemon/:id', (req, res) => {
-  const { id } = req.params;
-  getUserPokemon(id).then(pokeCollection => res.send(pokeCollection))
-    .catch(err => console.error(err));
+router.get("/pokemon/:id", (req, res) => {
+    const { id } = req.params;
+    getUserPokemon(id)
+        .then((pokeCollection) => res.send(pokeCollection))
+        .catch((err) => console.error(err));
 });
 
-router.get('/search', (req, res) => {
-  const { name } = req.query;
-  searchPokemon(name).then(pokeData => res.send(pokeData))
-    .catch(err => res.status(404).send(err));
+router.get("/search", (req, res) => {
+    const { name } = req.query;
+    searchPokemon(name)
+        .then((pokeData) => res.send(pokeData))
+        .catch((err) => res.status(404).send(err));
 });
 
-router.post('/pokebattle', (req, res) => {
-  const { pokemon, leadPokemon, userId } = req.body;
-  const victorPokemon = battle(pokemon, leadPokemon);
-  if (victorPokemon.name !== pokemon.name) {
-    gainBattleExperience(leadPokemon).then((pokeLevelGain) => {
-      addPokemonToCollection(userId, pokemon).then(() => {
+router.post("/firstPokemon", (req, res) => {
+    const { pokemon, userId } = req.body;
+    addPokemonToCollection(userId, pokemon).then(() => {
         getUserPokemon(userId).then((pokeCollection) => {
-          pokeCollection.push(pokeLevelGain);
-          pokeCollection.unshift('Caught!');
-          res.send(pokeCollection);
+            pokeCollection.unshift("You caught your first Pokemon!");
+            res.send(pokeCollection);
         });
-      })
-        .catch(err => console.error(err));
     });
-  } else {
-    getUserPokemon(userId).then((pokeCollection) => {
-      pokeCollection.unshift('You lost :(');
-      res.send(pokeCollection);
-    })
-      .catch(err => console.error(err));
-  }
+});
+
+router.post("/pokebattle", (req, res) => {
+    const { pokemon, leadPokemon, userId } = req.body;
+    const victorPokemon = battle(pokemon, leadPokemon);
+    if (victorPokemon.name !== pokemon.name) {
+        gainBattleExperience(leadPokemon).then((pokeLevelGain) => {
+            addPokemonToCollection(userId, pokemon)
+                .then(() => {
+                    getUserPokemon(userId).then((pokeCollection) => {
+                        pokeCollection.push(pokeLevelGain);
+                        pokeCollection.unshift("Caught!");
+                        res.send(pokeCollection);
+                    });
+                })
+                .catch((err) => console.error(err));
+        });
+    } else {
+        getUserPokemon(userId)
+            .then((pokeCollection) => {
+                pokeCollection.unshift("You lost :(");
+                res.send(pokeCollection);
+            })
+            .catch((err) => console.error(err));
+    }
 });
 
 module.exports.pokeRouter = router;
